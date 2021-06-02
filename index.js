60b394279ef7faf26b142b13e21e20756a547665
Luis Martinez <luis_mmartinez@live.com>
2021-06-02 19:31:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, callback) {
+  let result = [];
+  array.forEach( element => result.push(callback(element)) );
+  return result;
+}
+
+function reduce(array, callback, starting){
+    let r = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < array.length; i++) {
+      r = callback(array[i], r)
+    }
+
+    return r;
+  } 
+  
